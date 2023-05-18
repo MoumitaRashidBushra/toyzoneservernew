260586be805e18479e1854ac3e256cb438c4cb22
MoumitaRashidBushra <moumitarashidbushra@gmail.com>
2023-05-19 01:32:28 +0600

    Deleted options done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -50,6 +50,14 @@ async function run() {
             const result = await addAToyCollection.insertOne(addToy);
             res.send(result);
         })
+
+        app.delete('/addAToys/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await addAToyCollection.deleteOne(query);
+            res.send(result);
+        })
+
 
 
 
