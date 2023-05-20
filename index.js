d82ba30b45f088277b0fdd37970f722b259ee12e
MoumitaRashidBushra <moumitarashidbushra@gmail.com>
2023-05-20 10:23:46 +0600

    vercel.json file add

--- index.js
+++ index.js
@@ -28,7 +28,8 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
+        client.connect();
 
 
         const addAToyCollection = client.db('toyZone').collection('addAToy');
